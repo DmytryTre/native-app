752fc37d36fcc322e97dd4471a4ab4d1edbb8393
tretyak.da <TretyakDA.it@relef.ru>
2026-06-02 13:56:32 +0300

    add lint

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,29 +1,29 @@
 export const Colors = {
-  black: "#000000",
-  warmBrown: "#C67C4E",
-  warmBrownHover: "#A76237",
-  white: "#ffffff",
-  darkGray: "#A9A9A9"
+    black: '#000000',
+    warmBrown: '#C67C4E',
+    warmBrownHover: '#A76237',
+    white: '#ffffff',
+    darkGray: '#A9A9A9',
 }
 
 export const Gaps = {
-  g50: 50
+    g50: 50,
 }
 
 export const Radius = {
-  br16: 16
+    br16: 16,
 }
 
 export const Padding = {
-  p30: 30
+    p30: 30,
 }
 
 export const Dimensions = {
-  h62: 63
+    h62: 63,
 }
 
 export const FontSize = {
-  fs16: 16,
-  fs14: 14,
-  fs34: 34
+    fs16: 16,
+    fs14: 14,
+    fs34: 34,
 }
